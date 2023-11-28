c19c43e703315e284022df7a80bddb5792d684f2
MylamooriThanuja <104723321+MylamooriThanuja@users.noreply.github.com>
2023-11-28 11:21:47 +0530

    first commit

---
+++ src/main/java/com/project/Ebookmaker/EBookmakerApplication.java
@@ -0,0 +1,21 @@
+package com.project.Ebookmaker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EBookmakerApplication {
+	
+	
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(EBookmakerApplication.class);
+
+		logger.info("Application started");
+		SpringApplication.run(EBookmakerApplication.class, args);
+	}
+
+}
